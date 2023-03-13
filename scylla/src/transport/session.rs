137356a3f4cbad9db6cd37d09d19daeb58c499e4
Piotr Dulikowski <piodul@scylladb.com>
2023-03-13 14:04:10 +0100

    session: de-genericise internal query/exec functions
    The query/execute/batch statements are generic over the statement.
    They started by converting the statement to corresponding type
    (query/execute/batch) and then continued without the need for generics.
    However, those functions used to be non-trivial and would have to be
    monomorphised for every type of the arguments passed to the method,
    increasing compilation time more than necessary.
    
    Now that most of the implementation was moved to do_query etc. methods,
    we can restrict the generic part to the public query/execute/batch
    methods which convert the input statement to required type and then call
    the non-generic do_query etc. methods. This commit does just that -
    de-genericises do_query and friends, while leaving query and friends
    generic as they used to.
    
    Co-authored-by: Wojciech Przytuła <wojciech.przytula@scylladb.com>

--- scylla/src/transport/session.rs
+++ scylla/src/transport/session.rs
@@ -655,7 +655,7 @@ impl GenericSession<LegacyDeserializationApi> {
         query: impl Into<Query>,
         values: impl SerializeRow,
     ) -> Result<LegacyRowIterator, QueryError> {
-        self.do_query_iter(query, values).await
+        self.do_query_iter(query.into(), values).await
     }
 
     /// Execute a prepared statement. Requires a [PreparedStatement]
@@ -817,7 +817,7 @@ impl GenericSession<LegacyDeserializationApi> {
         prepared: impl Into<PreparedStatement>,
         values: impl SerializeRow,
     ) -> Result<LegacyRowIterator, QueryError> {
-        self.do_execute_iter(prepared, values).await
+        self.do_execute_iter(prepared.into(), values).await
     }
 
     /// Perform a batch query\
@@ -1171,11 +1171,9 @@ where
 
     async fn do_query_iter(
         &self,
-        query: impl Into<Query>,
+        query: Query,
         values: impl SerializeRow,
     ) -> Result<LegacyRowIterator, QueryError> {
-        let query: Query = query.into();
-
         let execution_profile = query
             .get_execution_profile_handle()
             .unwrap_or_else(|| self.get_default_execution_profile_handle())
@@ -1440,10 +1438,9 @@ where
 
     async fn do_execute_iter(
         &self,
-        prepared: impl Into<PreparedStatement>,
+        prepared: PreparedStatement,
         values: impl SerializeRow,
     ) -> Result<LegacyRowIterator, QueryError> {
-        let prepared = prepared.into();
         let serialized_values = prepared.serialize_values(&values)?;
 
         let execution_profile = prepared
