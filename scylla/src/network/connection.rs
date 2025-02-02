117e8d7e1bed026a0f357613fc26864170da75f1
Wojciech Przytuła <wojciech.przytula@scylladb.com>
2025-02-02 21:01:17 +0100

    cloud: support rustls
    rustls is now supported in cloud, too. The choice between rustls and
    openssl is done based on enabled features:
    - if openssl-010 feature is enabled, openssl is used for cloud;
    - else if rustls-023 feature is enabled, rustls is used for cloud;
    - else compile error is raised.
    
    It is subject to discussion in the PR is such logic based on features
    is acceptable.
    
    Co-authored-by: Andres Medina <andresnrx@gmail.com>

--- scylla/src/network/connection.rs
+++ scylla/src/network/connection.rs
@@ -328,6 +328,8 @@ mod tls_config {
         #[cfg(feature = "rustls-023")]
         Rustls023 {
             connector: tokio_rustls::TlsConnector,
+            #[cfg(feature = "cloud")]
+            sni: Option<rustls::pki_types::ServerName<'static>>,
         },
     }
 
@@ -342,6 +344,12 @@ mod tls_config {
         OpenSsl010ErrorStack(#[from] openssl::error::ErrorStack),
         #[cfg(feature = "openssl-010")]
         OpenSsl010Error(#[from] openssl::ssl::Error),
+        #[cfg(feature = "rustls-023")]
+        InvalidName(#[from] rustls::pki_types::InvalidDnsNameError),
+        #[cfg(feature = "rustls-023")]
+        PemParse(#[from] rustls::pki_types::pem::Error),
+        #[cfg(feature = "rustls-023")]
+        Rustls023(#[from] rustls::Error),
     }
 
     impl From<TlsError> for io::Error {
@@ -358,6 +366,12 @@ mod tls_config {
                         std::io::Error::new(io::ErrorKind::Other, format!("{:?}", e))
                     })
                 }),
+                #[cfg(feature = "rustls-023")]
+                TlsError::InvalidName(e) => io::Error::new(io::ErrorKind::Other, e),
+                #[cfg(feature = "rustls-023")]
+                TlsError::PemParse(e) => io::Error::new(io::ErrorKind::Other, e),
+                #[cfg(feature = "rustls-023")]
+                TlsError::Rustls023(e) => io::Error::new(io::ErrorKind::Other, e),
             }
         }
     }
@@ -406,8 +420,19 @@ mod tls_config {
                 #[cfg(feature = "rustls-023")]
                 TlsContext::Rustls023(ref config) => {
                     let connector = tokio_rustls::TlsConnector::from(config.clone());
+                    #[cfg(feature = "cloud")]
+                    let sni = self
+                        .sni
+                        .as_deref()
+                        .map(rustls::pki_types::ServerName::try_from)
+                        .transpose()?
+                        .map(|s| s.to_owned());
 
-                    Tls::Rustls023 { connector }
+                    Tls::Rustls023 {
+                        connector,
+                        #[cfg(feature = "cloud")]
+                        sni,
+                    }
                 }
             };
 
@@ -1599,8 +1624,16 @@ impl Connection {
                     .await);
                 }
                 #[cfg(feature = "rustls-023")]
-                tls_config::Tls::Rustls023 { connector } => {
+                tls_config::Tls::Rustls023 {
+                    connector,
+                    #[cfg(feature = "cloud")]
+                    sni,
+                } => {
                     use rustls::pki_types::ServerName;
+                    #[cfg(feature = "cloud")]
+                    let server_name =
+                        sni.unwrap_or_else(|| ServerName::IpAddress(node_address.into()));
+                    #[cfg(not(feature = "cloud"))]
                     let server_name = ServerName::IpAddress(node_address.into());
                     let stream = connector.connect(server_name, stream).await?;
                     return Ok(spawn_router_and_get_handle(
